22a6f369f5369ab352f0e95f38411f79b7aecc21
David Krmpotic <david.krmpotic@gmail.com>
2016-02-25 05:03:58 +0100

    fix when searching for a dep on a single project

--- deps.js
+++ deps.js
@@ -7,8 +7,6 @@ var Projects = require('./projects')
 let args = process.argv.slice(2)
 let dep = args[0]
 
-let project = new Projects().info(process.cwd())
-
 function hasDep(project, dep) {
   if(project.dependencies) {
     let deps = Object.keys(project.dependencies)
@@ -16,49 +14,51 @@ function hasDep(project, dep) {
   }
 }
 
-function showProject(project) {
-  let path = process.cwd()
+function showProject(project, dep) {
+  if((dep && hasDep(project, dep)) || !dep) {
+    let path = process.cwd()
 
-  console.log(colors.cyan(`<${project.name}>`))
+    console.log(colors.cyan(`<${project.name}>`))
 
-  if (project.dependencies) {
-    let deps = Object.keys(project.dependencies)
-    console.log(`${colors.yellow(deps.join(', '))}`)
+    if (project.dependencies) {
+      let deps = Object.keys(project.dependencies)
+      console.log(`${colors.yellow(deps.join(', '))}`)
 
-    if(dep) {
-      for(let match of deps.filter(d => d.toLowerCase().indexOf(dep.toLowerCase()) > -1)) {
-        console.log(colors.green(`https://www.npmjs.com/package/${match}`))
+      if(dep) {
+        for(let match of deps.filter(d => d.toLowerCase().indexOf(dep.toLowerCase()) > -1)) {
+          console.log(colors.green(`https://www.npmjs.com/package/${match}`))
+        }
       }
     }
-  }
 
-  // check for linked npm projects
-  let modulesPath = `${project.path}/node_modules`
-  if(util.isDirectory(modulesPath)) {
-    fs.readdir(modulesPath, function(err, dirs) {
-      for(let dir of dirs) {
-        fs.lstat(`${modulesPath}/${dir}`, function(err, stats) { //fs.statSync can't read isSymbolicLink()
-          if (err) throw err;
-          if (stats.isSymbolicLink()) {
-            console.log(colors.red(`<${project.name}> linked package: ${dir}`))
-          }
-        })
-      }
-    })
-  }
+    // check for linked npm projects
+    let modulesPath = `${project.path}/node_modules`
+    if(util.isDirectory(modulesPath)) {
+      fs.readdir(modulesPath, function(err, dirs) {
+        for(let dir of dirs) {
+          fs.lstat(`${modulesPath}/${dir}`, function(err, stats) { //fs.statSync can't read isSymbolicLink()
+            if (err) throw err;
+            if (stats.isSymbolicLink()) {
+              console.log(colors.red(`<${project.name}> linked package: ${dir}`))
+            }
+          })
+        }
+      })
+    }
 
-  console.log("")
+    console.log("")
+  }
 }
 
+let project = new Projects().info(process.cwd())
+
 if(project) {
-  showProject(project)
+  showProject(project, dep)
 } else {
   let projects = new Projects().scan(process.cwd())
   if(projects.length) {
     for(let project of projects) {
-      if((dep && hasDep(project, dep)) || !dep) {
-        showProject(project)
-      }
+      showProject(project, dep)
     }
   }
 }
